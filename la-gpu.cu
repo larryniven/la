30256ed91d3248eb60287648059fadb1cef56201
Hao Tang <haotang@ttic.edu>
2015-12-16 16:42:55 -0600

    Allow copy from host to device

---
+++ la-gpu.cu
@@ -0,0 +1,180 @@
+#include "la/la-gpu.h"
+#include <cmath>
+#include <cassert>
+#include <cublas_v2.h>
+#include <cblas.h>
+
+namespace la {
+
+    namespace gpu {
+
+        device device::d = device();
+
+        device::device()
+        {
+            cublasCreate(&handle);
+        }
+
+        device::~device()
+        {
+            cublasDestroy(handle);
+        }
+
+        device& device::get_instance()
+        {
+            return d;
+        }
+
+        cublasHandle_t& device::get_handle()
+        {
+            return get_instance().handle;
+        }
+
+        void imul(vector<double>& u, double d)
+        {
+            cublasDscal(device::get_handle(), u.size(), &d, u.data(), 1);
+        }
+
+        void iadd(vector<double>& u, vector<double> const& v)
+        {
+            assert(u.size() == v.size());
+
+            double alpha = 1;
+            cublasDaxpy(device::get_handle(), u.size(), &alpha, v.data(), 1, u.data(), 1);
+        }
+
+        void isub(vector<double>& u, vector<double> const& v)
+        {
+            assert(u.size() == v.size());
+
+            double alpha = -1;
+            cublasDaxpy(device::get_handle(), u.size(), &alpha, v.data(), 1, u.data(), 1);
+        }
+
+        void imul(vector<double>& u, vector<double> const& v)
+        {
+            assert(u.size() == v.size());
+
+            for (int i = 0; i < v.size(); ++i) {
+                u(i) *= v(i);
+            }
+        }
+
+        void idiv(vector<double>& u, vector<double> const& v)
+        {
+            assert(u.size() == v.size());
+
+            for (int i = 0; i < v.size(); ++i) {
+                u(i) /= v(i);
+            }
+        }
+
+        vector<double> add(
+            vector<double> u,
+            vector<double> const& v)
+        {
+            iadd(u, v);
+            return u;
+        }
+
+        double norm(vector<double> const& v)
+        {
+            double result = 0;
+            cublasDnrm2(device::get_handle(), v.size(), v.data(), 1, &result);
+            return result;
+        }
+
+        double dot(vector<double> const& u, vector<double> const& v)
+        {
+            assert(u.size() == v.size());
+
+            double result = 0;
+            cublasDdot(device::get_handle(), u.size(), u.data(), 1, v.data(), 1, &result);
+            return result;
+        }
+
+        vector<double> logistic(vector<double> const& v)
+        {
+            vector<double> result;
+            result.resize(v.size());
+
+            for (int i = 0; i < v.size(); ++i) {
+                result(i) = 1 / (1 + std::exp(-v(i)));
+            }
+
+            return result;
+        }
+
+        void iadd(matrix<double>& u, matrix<double> const& v)
+        {
+            assert(u.rows() == v.rows());
+            assert(u.cols() == v.cols());
+
+            for (int i = 0; i < u.rows(); ++i) {
+                for (int j = 0; j < u.cols(); ++j) {
+                    u(i, j) += v(i, j);
+                }
+            }
+        }
+
+        void isub(matrix<double>& u, matrix<double> const& v)
+        {
+            assert(u.rows() == v.rows());
+            assert(u.cols() == v.cols());
+
+            for (int i = 0; i < u.rows(); ++i) {
+                for (int j = 0; j < u.cols(); ++j) {
+                    u(i, j) -= v(i, j);
+                }
+            }
+        }
+
+        vector<double> mult(
+            matrix<double> const& u,
+            vector<double> const& v)
+        {
+            vector<double> result;
+            result.resize(u.rows());
+
+            double alpha = 1;
+            double beta = 1;
+            cublasDgemv(device::get_handle(), CUBLAS_OP_N,
+                u.rows(), u.cols(), &alpha, u.data(), u.cols(),
+                v.data(), 1, &beta, result.data(), 1);
+
+            return result;
+        }
+
+        vector<double> lmult(
+            matrix<double> const& u,
+            vector<double> const& v)
+        {
+            vector<double> result;
+            result.resize(u.rows());
+    
+            double alpha = 1;
+            double beta = 1;
+            cublasDgemv(device::get_handle(), CUBLAS_OP_T,
+                u.rows(), u.cols(), &alpha, u.data(), u.cols(),
+                v.data(), 1, &beta, result.data(), 1);
+    
+            return result;
+        }
+ 
+        vector<double> dyadic_prod(vector<double> const& a,
+            vector<double> const& b)
+        {
+            vector<double> result;
+
+            result.resize(a.size() * b.size());
+
+            for (int i = 0; i < a.size(); ++i) {
+                for (int j = 0; j < b.size(); ++j) {
+                    result(i * a.size() + j) = a(i) * b(j);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ la-gpu.h
@@ -0,0 +1,223 @@
+#ifndef LA_GPU_H
+#define LA_GPU_H
+
+#include "la/la.h"
+#include <cublas_v2.h>
+#include <vector>
+#include <cassert>
+
+namespace la {
+
+    namespace gpu {
+
+        struct device {
+            static device d;
+
+            device();
+            ~device();
+
+            cublasHandle_t handle;
+
+            static device& get_instance();
+            static cublasHandle_t& get_handle();
+        };
+
+        template <class T>
+        struct vector {
+
+            vector()
+                : data_(nullptr)
+            {}
+
+            explicit vector(la::vector<T> const& v)
+            {
+                 cudaMalloc(&data_, sizeof(T) * v.size());
+                 cublasSetVector(v.size(), sizeof(T), v.data(), 1, data_, 1);
+            }
+
+            vector(vector<T>&& v)
+            {
+                 data_ = v.data_;
+                 v.data_ = nullptr;
+                 size_ = v.size_;
+            }
+
+            vector(vector<T> const& v)
+            {
+                cudaMalloc(&data_, sizeof(T) * v.size());
+                cudaMemcpy(data_, v.data(), sizeof(T) * v.size(), cudaMemcpyDeviceToDevice);
+                size_ = v.size();
+            }
+
+            vector<T>& operator=(vector<T> const& v)
+            {
+                cudaFree(data_);
+                cudaMalloc(&data_, sizeof(T) * v.size());
+                cudaMemcpy(data_, v.data(), sizeof(T) * v.size(), cudaMemcpyDeviceToDevice);
+                size_ = v.size();
+                return *this;
+            }
+
+            vector<T>& operator=(vector<T>&& v)
+            {
+                data_ = v.data_;
+                v.data_ = nullptr;
+                size_ = v.size_;
+
+                return *this;
+            }
+
+            ~vector()
+            {
+                cudaFree(data_);
+            }
+
+            T* data()
+            {
+                return data_;
+            }
+
+            T const* data() const
+            {
+                return data_;
+            }
+
+            unsigned int size() const
+            {
+                return size_;
+            }
+
+            void resize(unsigned int size, T value = 0)
+            {
+                T* result;
+                cudaMalloc(&result, sizeof(T) * size);
+                cudaMemset(result, value, size_);
+                cudaMemcpy(result, data_, sizeof(T) * size_, cudaMemcpyDeviceToDevice);
+                cudaFree(data_);
+                data_ = result;
+                size_ = size;
+            }
+
+            T& operator()(int i)
+            {
+                return data_[i];
+            }
+
+            T const& operator()(int i) const
+            {
+                return data_[i];
+            }
+
+            T& at(int i)
+            {
+                assert(i < size_);
+                return data_[i];
+            }
+
+            T const& at(int i) const
+            {
+                assert(i < size_);
+                return data_[i];
+            }
+
+        private:
+            T* data_;
+            unsigned int size_;
+        };
+
+        template <class T>
+        struct matrix {
+
+            matrix()
+            {}
+
+            matrix(la::matrix<T> const& m)
+                : vec_(la::to_vector(m.vec_))
+            {}
+
+            T* data()
+            {
+                return vec_.data();
+            }
+
+            T const* data() const
+            {
+                return vec_.data();
+            }
+
+            unsigned int rows() const
+            {
+                return rows_;
+            }
+
+            unsigned int cols() const
+            {
+                return cols_;
+            }
+
+            void resize(int rows, int cols, T value = 0)
+            {
+                vec_.resize(cols * rows, value);
+                rows_ = rows;
+                cols_ = cols;
+            }
+
+            T& operator()(unsigned int r, unsigned int c)
+            {
+                return vec_(c * rows_ + r);
+            }
+
+            T const& operator()(unsigned int r, unsigned int c) const
+            {
+                return vec_(c * rows_ + r);
+            }
+
+            T& at(unsigned int r, unsigned int c)
+            {
+                return vec_.at(c * rows_ + r);
+            }
+
+            T const& at(unsigned int r, unsigned int c) const
+            {
+                return vec_.at(c * rows_ + r);
+            }
+
+        private:
+            vector<T> vec_;
+            unsigned int rows_;
+            unsigned int cols_;
+        };
+
+        void imul(vector<double>& u, double d);
+
+        void iadd(vector<double>& u, vector<double> const& v);
+        void isub(vector<double>& u, vector<double> const& v);
+        void imul(vector<double>& u, vector<double> const& v);
+        void idiv(vector<double>& u, vector<double> const& v);
+
+        vector<double> add(vector<double> u,
+            vector<double> const& v);
+
+        double norm(std::vector<double> const& v);
+
+        double dot(vector<double> const& u, vector<double> const& v);
+
+        vector<double> logistic(vector<double> const& v);
+
+        void iadd(matrix<double>& u, matrix<double> const& v);
+        void isub(matrix<double>& u, matrix<double> const& v);
+
+        vector<double> mult(matrix<double> const& a,
+            vector<double> const& v);
+
+        vector<double> lmult(
+            matrix<double> const& u,
+            vector<double> const& v);
+
+        vector<double> dyadic_prod(vector<double> const& a,
+            vector<double> const& b);
+
+    }
+}
+
+#endif
